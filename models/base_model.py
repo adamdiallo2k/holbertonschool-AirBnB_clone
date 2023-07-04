bb8315ae0bd7f196d7de281b138a587e6911e43b
AdamDiallo <adamdiallo2k@gmail.com>
2023-07-04 23:50:33 +0200

    test

--- models/base_model.py
+++ models/base_model.py
@@ -18,5 +18,6 @@ class BaseModel:
     
     def to_dict(self):
         self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = datetime.isoformat()
-        self.__dict__["updated_at"] = datetime.isoformat()
+        self.__dict__["created_at"] = datetime.isoformat(self.__dict__["updated_at"])
+        self.__dict__["updated_at"] = datetime.isoformat(self.__dict__["updated_at"])
+        return self.__dict__
