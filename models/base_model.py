7b0a31b168e38a5addbdf38a0c14b080230c5d05
AdamDiallo <adamdiallo2k@gmail.com>
2023-07-05 21:02:03 +0200

    test

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,14 @@ class BaseModel:
         self.updated_at = datetime.now()
     
     def __str__(self):
-        return f'[{self.__class__.__name__}] ({self.id} <{self.__dict__}>)'
+        return f'[{self.__class__.__name__}] ({self.id} {self.__dict__})'
     
     def save(self):
         self.updated_at = datetime.now()
     
     def to_dict(self):
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = self.created_at.isoformat()
-        self.__dict__["updated_at"] = self.updated_at.isoformat()
-        return self.__dict__
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
