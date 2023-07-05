f6a60a8d5e18c453731ce37fa8be09aea1c85a38
samdaphbynet <lj8@hotmail.fr>
2023-07-05 22:27:06 +0200

    update base_model to create an instance with dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -6,17 +6,23 @@ from datetime import datetime
 
 class BaseModel:
     """commmented class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id} {self.__dict__})'
-    
+
     def save(self):
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
