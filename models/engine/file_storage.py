21c5bff0fd4cbbd68b70f5ba9eadce6c99fc0da9
samdaphbynet <lj8@hotmail.fr>
2023-07-06 12:34:35 +0200

    new modification in new brach

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,25 +4,30 @@ import json
 
 
 class FileStorage:
-    __file_path = ""
+    __file_path = "file.json"
     __objects = {}
 
-    def __init__(self, file_path, objects):
-        __file__path = file_path
-        __objects = objects
-    
     def all(self):
-        return self.__objects.__dict__()
-    
+        return self.__objects
+
     def new(self, obj):
-        self.__objects[f"{obj.__class__.__name__}.{id(obj)}"] = obj
-    
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
     def save(self):
-            with open(self.__file_path, "w") as write_file:
-                json.dump(self.__objects, write_file)
-    
+        serialized_objects = FileStorage
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+
     def reload(self):
-        if self.__file_path is not None:
-             with open(self.__file_path, 'w') as read_file:
-                 data = json.load(read_file)
-        self.__objects = data
+        try:
+            with open(self.__file_path, 'r') as file:
+                serialized_objects = json.load(file)
+                for key, obj_dict in serialized_objects.items():
+                    class_name, obj.id = key.split('.')
+                    obj = globals()[class_name](**obj_dict)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
