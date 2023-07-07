7521a285bfd35231c202055dad689f245119ee40
AdamDiallo <adamdiallo2k@gmail.com>
2023-07-07 16:49:45 +0200

    task user

--- models/state.py
+++ models/state.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""commented function"""
 from models.base_model import BaseModel
+
+
 class State(BaseModel):
+    """commented class"""
     name = ""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
