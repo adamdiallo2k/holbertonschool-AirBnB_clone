479ade809e06f69f76efa7ff2df8ea96891feb7f
AdamDiallo <adamdiallo2k@gmail.com>
2023-07-09 22:39:48 +0200

    task 10

--- console.py
+++ console.py
@@ -4,13 +4,27 @@ import cmd
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class that includes methods for the HBNB command interpreter."""
 
     prompt = '(hbnb) '
-    classes = {"BaseModel": BaseModel, "User": User}
+    classes = {
+        "BaseModel": BaseModel, 
+        "User": User, 
+        "Place": Place, 
+        "State": State, 
+        "City": City, 
+        "Amenity": Amenity, 
+        "Review": Review
+    }
+
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
