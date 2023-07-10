e8a653bdac843eade7e9954ce3205b190cae90e5
samdaphbynet <lj8@hotmail.fr>
2023-07-11 00:13:26 +0200

    update

--- console.py
+++ console.py
@@ -2,7 +2,6 @@
 """HBNBCommand class for the console"""
 import cmd
 import shlex
-from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -31,6 +30,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id."""
+        from models import storage
         if not arg:
             print("** class name missing **")
             return
@@ -45,6 +45,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         """Prints the string representation of an instance based on the class name and id."""
+        from models import storage
         if not arg:
             print("** class name missing **")
             return
@@ -67,6 +68,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id (save the change into the JSON file)."""
+        from models import storage
         if not arg:
             print("** class name missing **")
             return
@@ -91,6 +93,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         """Prints all string representation of all instances based or not on the class name."""
+        from models import storage
         all_instances = storage.all()
         if not arg:
             print([str(instance) for instance in all_instances.values()])
@@ -106,6 +109,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, arg):
         """Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file)."""
+        from models import storage
         if not arg:
             print("** class name missing **")
             return
